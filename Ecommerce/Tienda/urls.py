4abf045a21131f4245a2abc36af9fdcd7f449596
Mallic Tesla <el_mallic@hotmail.com>
2024-02-11 16:10:30 -0300

    nuevo proyecto react

--- Ecommerce/Tienda/urls.py
+++ Ecommerce/Tienda/urls.py
@@ -2,10 +2,12 @@ from django.conf import settings
 from django.urls import re_path
 from django.contrib import admin
 from django.urls import path, re_path, include
-from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from rest_framework import permissions
 
+# JWT
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
 schema_view = get_schema_view(
@@ -22,6 +24,12 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
+    #  JWT
+    #   esta ruta sirve para retornar y refrescar el token de acseso para un usuario y contraseña que se le envia
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    #   refresca el toquen que estas usando
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
     # swagger
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
@@ -33,4 +41,7 @@ urlpatterns = [
     path ("usuario/", include("usuarios.api.routers")),
     # producto
     path ("productos/", include ("productos.api.routers")),
+    # gestion_gastos
+    path ("gasto/", include ("gestion_gastos.api.routers")),
+
 ]
