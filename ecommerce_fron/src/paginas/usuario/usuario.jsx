31dfe6fd625f65f4696b55cbcbdae548de7aea53
Mallic Tesla <el_mallic@hotmail.com>
2024-02-19 23:20:15 -0300

    continuando con usuario

--- ecommerce_fron/src/paginas/usuario/usuario.jsx
+++ ecommerce_fron/src/paginas/usuario/usuario.jsx
@@ -1,55 +1,64 @@
-import React, { useState, useEffect } from "react";
+import { BrowserRouter as useNavigate  } from "react-router-dom";
+import React, { useState } from "react";
 import axios from "axios";
 
-function Usuario ({ location }) {
+import Usuarios from "./usuarios";
+
+function Usuario () {
     const [detalles, setDetalles] = useState([]);
     const [usuarioId, setUsuarioId] = useState();
-    // const usuarioId = location.state.usuarioId;
+    const [mostrarUsuarios, setMostrarUsuarios] = useState(true);
+    // const navigate = useNavigate();
 
     const obtenerUsuarioPorId = () => {
         axios.get (`http://127.0.0.1:8000/usuario/usuario/${usuarioId}/`).then ((res) => {
             const data = res.data;
             setDetalles ([data]);
+            setMostrarUsuarios (false);  
         })
         .catch((err) => {});
     };
 
-
-    useEffect(() => {
-        // Verifica si location está definido y tiene la propiedad state
-        if (location && location.state && location.state.usuarioId) {
-            setUsuarioId(location.state.usuarioId);
-            obtenerUsuarioPorId();
-        }
-    }, [location]);
+    // {mostrarUsuarios ? ( navigate ("/usuaris", usuarioId ) ) : (<h2> Usuario </h2>)}
+    // navigate("/usuaris", usuarioId );
 
     return (
         <div>
-            <h1> Vista de Usuarios </h1>
-            <hr />
-
             <div>
-                <label> ID del Usuario: </label>
-                <input type="text" value={usuarioId} onChange = {(e) => setUsuarioId (e.target.value)}/>
-                <button onClick = {obtenerUsuarioPorId}> Mostrar Usuario por ID </button>
-            </div>
-            <hr />
+                <h1> Vista de Usuarios </h1>
+                <hr />
 
-            <h2> Usuario </h2>
-            <hr />
-
-            {detalles.map ((output, id) => (
-                <div key = {id}>
-                    <div>
-                        <h3> Nombre de usuario : {output.nombre_usuario}</h3>
-                        <h4> ID : {output.id}</h4>
-                        <h4> Nombre : {output.nombre}</h4>
-                        <h4> Apellido : {output.apellido}</h4>
-                        <h4> Email : {output.email}</h4>
-                        <h4> Grupos : {output.groups}</h4>
-                    </div>
+                <div>
+                    <label> ID del Usuario: </label>
+                    <input type="text" value={usuarioId} onChange = {(e) => setUsuarioId (e.target.value)}/>
+                    <button onClick = {obtenerUsuarioPorId}> Mostrar Usuario por ID </button>
                 </div>
-            ))}
+                <hr />
+            </div>
+
+            {mostrarUsuarios ? (
+                <Usuarios />
+                // <h1> asdasd </h1>
+            ) : (
+                <div>
+
+                    <h2> Usuario </h2>
+                    <hr />
+
+                    {detalles.map ((output, id) => (
+                        <div key = {id}>
+                            <div>
+                                <h3> Nombre de usuario : {output.nombre_usuario}</h3>
+                                <h4> ID : {output.id}</h4>
+                                <h4> Nombre : {output.nombre}</h4>
+                                <h4> Apellido : {output.apellido}</h4>
+                                <h4> Email : {output.email}</h4>
+                                <h4> Grupos : {output.groups}</h4>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            )}
         </div>
     );
 }
--- ecommerce_fron/src/paginas/usuario/usuarios.jsx
+++ ecommerce_fron/src/paginas/usuario/usuarios.jsx
@@ -1,48 +1,26 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Link, Route, Routes, useNavigate  } from "react-router-dom";
 import axios from "axios";
 
-import Usuario from "./usuario";
 
 function Usuarios () {
     const [detalles, setDetalles] = useState([]);
-    const [usuarioId, setUsuarioId] = useState();
-    const [mostrarUsuarios, setMostrarUsuarios] = useState(true);
-    const navigate = useNavigate(); // Importa useNavigate desde react-router-dom y declara una instancia de navigate.
-
-
-    const obtenerUsuarioPorId = () => {
-        setMostrarUsuarios (false);                             // Oculta la lista de usuarios al mostrar uno específico por ID.
-        // Cambia la ruta a /usuario y pasa el usuarioId como state
-        navigate("/usuario", { state: { usuarioId } });
-    };
 
     const obtenerUsuarios = () => {
         axios.get (`http://127.0.0.1:8000/usuario/usuario/`).then ((res) => {
             const data = res.data;
             setDetalles (data);
-            // setMostrarUsuarios (true);                           // Muestra la lista de usuarios al obtener todos.
         })
         .catch((err) => {});
     };
 
     useEffect(() => {
-        obtenerUsuarios(); // Llama a obtenerUsuarios cuando el componente se monta
+        obtenerUsuarios();
     }, []);
 
     return (
         <div>
 
             <div>
-                <h1> Vista de Usuarios </h1>
-                <hr />
-
-                <div>
-                    <label> ID del Usuario: </label>
-                    <input type="text" value={usuarioId} onChange = {(e) => setUsuarioId (e.target.value)}/>
-                    <button onClick = {obtenerUsuarioPorId}> Mostrar Usuario por ID </button>
-                </div>
-                <hr />
 
                 <h2> Usuarios </h2>
                 <hr />
